04a4a9304c84592cf2f6dd7cb58092ba4879cfcf
_tango <shurinskiy@mail.ru>
2023-09-09 21:51:42 +0600

    add slider to hero

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,9 +1,31 @@
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+import Swiper from 'swiper';
+import { Autoplay, EffectFade } from 'swiper/modules';
 
 (() => {
+	let header = document.querySelector('.header');
 
+	let swiper = new Swiper(".header__slider", {
+		modules: [ Autoplay, EffectFade ],
+		spaceBetween: 30,
+		loop: true,
+		speed: 2000,
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		autoplay: {
+			delay: 5000,
+		},
+		on: {
+			slideChange: function () {
+				// console.log(swiper.realIndex);
+				header.className = 'header';
+				header.classList.add(`header_type-${swiper.realIndex}`);
+			}
+		}
+	});
 	
 })();
