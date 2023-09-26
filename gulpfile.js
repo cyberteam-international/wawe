d1a800fe002fad2e4626dfadf836eecf4fedce1a
_tango <shurinskiy@mail.ru>
2023-09-26 18:01:54 +0600

    safari fix

--- gulpfile.js
+++ gulpfile.js
@@ -116,6 +116,7 @@ function styles() {
 			grid: true 
 		}))
 		// .pipe($.if(isProd, $.groupCssMediaQueries()))
+		// .pipe($.if(isProd, $.cleanCss({ level: { 2: { removeWhitespace: false }}})))
 		.pipe($.if(isProd, $.cleanCss({ level: 2 })))
 		.pipe($.if(isDev, $.sourcemaps.write()))
 		.pipe(gulp.dest(pth.pbl.root))
