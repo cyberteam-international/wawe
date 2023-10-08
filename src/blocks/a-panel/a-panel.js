9b8e65d5f18cb8073faf5badf09ba45f25a5f51f
_tango <shurinskiy@mail.ru>
2023-10-08 22:25:03 +0600

    before content

--- src/blocks/a-panel/a-panel.js
+++ src/blocks/a-panel/a-panel.js
@@ -1,18 +1,31 @@
 (() => {
 
-	const panel = document.querySelector('.a-panel__top');
-	const more = document.querySelector('.a-panel__controls #more');
-	const close = document.querySelector('.a-panel__t-close');
-	const controls_size = document.querySelectorAll('.a-panel__controls [name="size"]');
-	const controls_theme = document.querySelectorAll('.a-panel__controls [name="scheme"]');
-	const control_image = document.querySelector('.a-panel__controls [name="image"]');
+	const panel = document.querySelector('.a-panel');
+	if (!panel) return;
 
-	more.addEventListener('click', (e) => panel.classList.toggle('showed'));
-	close.addEventListener('click', (e) => panel.classList.remove('showed'));
+	const panel_top = panel.querySelector('.a-panel__top');
+	const more = panel.querySelector('.a-panel__controls #more');
+	const close = panel.querySelector('.a-panel__t-close');
+	const settings = document.querySelector('.a-header__settings');
+	const controls = panel.querySelectorAll('.a-panel__buttons [name="font"], .a-panel__controls [name="size"], .a-panel__controls [name="theme"], .a-panel__buttons [name="spacing"]');
+	const control_image = panel.querySelector('.a-panel__controls [name="image"]');
 
-	controls_theme.forEach((item) => {
+	more.addEventListener('click', (e) => panel_top.classList.toggle('showed'));
+	
+	close.addEventListener('click', (e) => {
+		panel_top.classList.remove('showed');
+		panel.classList.remove('showed');
+	});
+
+	settings.addEventListener('click', (e) => panel.classList.toggle('showed'));
+	
+	control_image.addEventListener('click', ({target: t}) => {
+		document.documentElement[((t.checked) ? 'set' : 'remove') + 'Attribute']('data-image', '');
+	});
+
+	controls.forEach((item) => {
 		item.addEventListener('click', (e) => {
-			document.documentElement.dataset.theme = item.id;
+			document.documentElement.dataset[item.name] = item.id;
 		});
 	});
 
