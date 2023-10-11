7c9277e54d3866a72750242509fa92daf20fff48
_tango <shurinskiy@mail.ru>
2023-10-11 19:53:17 +0600

    adaptive ready

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,7 @@
+import { scrollClassToggle } from "./libs/scroll";
 import "./polyfills.js";
 import "./blocks.js";
 
 /* Тут можно писать код общий для всего проекта и требующий единого пространства имен */
+
+scrollClassToggle({ class: 'showed' });
